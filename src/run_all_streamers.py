037a4ca1f899ee732711d2ebd69c1c4385860936
Christopher Collins <qemtek@gmail.com>
2019-08-17 11:53:55 +0200

    hotfix

--- src/run_all_streamers.py
+++ src/run_all_streamers.py
@@ -5,17 +5,25 @@ from configuration import football
 from configuration import project_dir
 from configuration import racing
 
-if racing:
+if racing and not football:
     subprocess.call(
         "python3 {}.py".format(
             os.path.join(project_dir, "src", "market_streaming_racing")
         ),
         shell=True,
     )
-if football:
+if football and not racing:
     subprocess.call(
         "python3 {}.py".format(
             os.path.join(project_dir, "src", "market_streaming_football")
+        ),
+        shell=True,
+    )
+if football and racing:
+    subprocess.call(
+        "python3 {}.py & {}.py".format(
+            os.path.join(project_dir, "src", "market_streaming_football"),
+            os.path.join(project_dir, "src", "market_streaming_racing"),
         ),
         shell=True,
     )
